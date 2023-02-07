5bc8e53ca38a7fd09e5a3be6406f18724cad6357
Abu-kawsar-rabby <rabbyafrad24@.com>
2023-02-07 08:43:55 +0600

    seceffnd commit

--- five.js
+++ five.js
@@ -2,7 +2,7 @@ function gemsToDiamond(number1, number2, number3){
     const friend1Diamond = number1 * 21;
     const friend2Diamond = number2 * 32;
     const friend3Diamond = number3 * 43;
-    const totalDiamond = friend1Diamond+friend2Diamond+friend3Diamond;
+    const totalDiamond = friend1Diamond + friend2Diamond + friend3Diamond;
     if(totalDiamond > 1000*2){
         const restDiamond = totalDiamond - 2000;
         return restDiamond;
